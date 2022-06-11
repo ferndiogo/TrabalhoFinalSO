4465e7973a1f31e8c9bc30509cb155c552569c5e
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2022-06-11 15:49:34 +0100

    Declaraçao de arrays buffers

--- ProjetoFinalSO/ProjetoFinalSO/TrabalhoFinalSO.c
+++ ProjetoFinalSO/ProjetoFinalSO/TrabalhoFinalSO.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <pthread.h>
+
+#define tamIN 20000
+#define tamCir 50
+#define tamOUT 19998
+
+int bfIN[tamIN];
+int bfCir[tamCir];
+int bfOUT[tamOUT];
 
 int main()
 {
