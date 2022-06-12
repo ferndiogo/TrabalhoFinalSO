456fe76f04c0b7202e64d8e3fdd8aaa4238fa457
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2022-06-12 17:50:31 +0100

    A funcionar (falta contadores)

--- ProjetoFinalSO/ProjetoFinalSO/TrabalhoFinalSO.c
+++ ProjetoFinalSO/ProjetoFinalSO/TrabalhoFinalSO.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define tamIN 100
-#define tamCir 2
-#define tamOUT 98
+#define tamIN 200
+#define tamCir 5
+#define tamOUT 198
 
 int bfIN[tamIN];
 int bfCir[tamCir];
@@ -22,13 +22,12 @@ pthread_mutex_t mutex;
 
 void* produtora(void* arg) {
 	int media;
-	while (posBfIN < tamIN - 2) {
+	while (posBfIN < tamIN - 3) {
 		sem_wait(&empty);
 		pthread_mutex_lock(&mutex);
 		//variável que guarda a média de três posições consecutivas
 		media = (bfIN[posBfIN] + bfIN[posBfIN + 1] + bfIN[posBfIN + 2]) / 3;
 		posBfIN++;
-
 
 		for (int i = 0; i < tamCir; i++) {
 			if (bfCir[i] == 0) {
@@ -44,7 +43,6 @@ void* produtora(void* arg) {
 		}
 		printf("\n");*/
 	}
-
 
 }
 
@@ -90,10 +88,13 @@ int main() {
 	sem_init(&empty, 0, tamCir);
 	sem_init(&full, 0, 0);
 
+	int countP1 = 0;
+	int countP2 = 0;
+	int countC1 = 0;
 	//cria as threads produtoras e consumidora
-	pthread_create(&PM_T1, NULL, produtora, NULL);
-	pthread_create(&PM_T2, NULL, produtora, NULL);
-	pthread_create(&CM_T, NULL, consumidora, NULL);
+	pthread_create(&PM_T1, NULL, produtora, countP1);
+	pthread_create(&PM_T2, NULL, produtora, countP2);
+	pthread_create(&CM_T, NULL, consumidora, countC1);
 
 	//executa as threads produtoras e consumidora até terminarem
 	pthread_join(PM_T1, NULL);
@@ -111,5 +112,9 @@ int main() {
 		printf("%d |", bfCir[i]);
 	}
 	printf("\n");
+
+	printf("%d |", countP1);
+	printf("%d |", countP2);
+	printf("%d |", countC1);
 
 }
